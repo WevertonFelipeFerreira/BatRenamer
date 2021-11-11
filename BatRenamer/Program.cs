6ba8b59aa9829f1b592721a38dfc9c163bb8ec2d
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2021-11-11 10:53:41 -0300

    First commit

---
+++ BatRenamer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BatRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string formatExample = "OriginalName.jpg;Renamed.jpg";
+            List<NameInfo> list = new List<NameInfo>();
+
+            bool cont = true;
+            Console.WriteLine($"\nEnter the original filename, then enter the name you want to rename, separated by a semicolon.EX: '{formatExample}'");
+            do
+            {
+                string c = Console.ReadLine();
+                if (string.IsNullOrEmpty(c) || !c.Contains(";") || !c.Contains("."))
+                {
+                    Console.WriteLine($"INVALID FORMAT! Format must be like: {formatExample}");
+                    break;
+                }
+
+                string[] fullString = c.Split(";");
+                list.Add(new NameInfo(fullString[0], fullString[1]));
+
+            } while (cont);
+
+        }
+    }
+}
